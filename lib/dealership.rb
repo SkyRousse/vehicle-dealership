5c759d0010f1f01b184700fed847701146e3b460
sr <skyrousse@gmail.com>
2016-07-07 20:47:11 -0700

    add method for adding vehicles into cars array in the dealership objects with passing specs

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -19,12 +19,30 @@ class Dealership
     @id
   end
 
+  define_method(:cars) do
+    @cars
+  end
+
+  define_singleton_method(:find) do |id|
+    found_dealership = nil
+    @@dealerships.each() do |dealership|
+      if dealership.id().eql?(id)
+        found_dealership = dealership
+      end
+    end
+    found_dealership
+  end
+
   define_singleton_method(:all) do
     @@dealerships
   end
 
   define_method(:save) do
     @@dealerships.push(self)
+  end
+
+  define_method(:add_vehicle) do |vehicle|
+    @cars.push(vehicle)
   end
 
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('dealership')
+require('vehicle')
 
 describe(Dealership) do
 
@@ -18,15 +19,46 @@ describe(Dealership) do
     it('saves an instance of a new Dealership object in the @@dealerships array') do
       test_dealership = Dealership.new("Johnny's Big Top")
       test_dealership.save()
-      expect(Dealership.all()).to(eq([test_dealership]))
+      test_dealership_2 = Dealership.new("Bob's Used Cars")
+      test_dealership_2.save()
+      expect(Dealership.all()).to(eq([test_dealership,test_dealership_2]))
     end
   end
 
   describe('#id') do
     it('returns the id of the dealership') do
       test_dealership = Dealership.new("Frank's Slick Wheelies")
+      test_dealership.save()
       test_dealership_2 = Dealership.new("Bob's Used Cars")
-      expect(test_dealership_2.id()).to(eq(1))
+      test_dealership_2.save()
+      expect(test_dealership_2.id()).to(eq(2))
+    end
+  end
+
+  describe('#cars') do
+    it('initially returns an empty array of cars for the dealership') do
+      test_dealership = Dealership.new("Patt's Perfect Picks")
+      test_dealership.save()
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('returns a dealership by its id number') do
+      test_dealership = Dealership.new("Tommy's djanky Jalopies")
+      test_dealership.save()
+      test_dealership_2 = Dealership.new("Jane's Comfort Cars")
+      test_dealership_2.save()
+      expect(Dealership.find(test_dealership.id())).to(eq(test_dealership))
+    end
+  end
+
+  describe('#add_vehicle') do
+    it("adds a new vehicle to a dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_dealership.add_vehicle(test_vehicle)
+      expect(test_dealership.cars()).to(eq([test_vehicle]))
     end
   end
 
