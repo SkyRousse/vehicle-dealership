45ace75113a842e092a2dff35f413029945088c6
sr <skyrousse@gmail.com>
2016-07-08 00:50:12 -0700

    fix route for success page and content after posting a new dealership by submitting form

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/vehicle')
+require('./lib/dealership')
 
 get('/') do
   erb(:index)
@@ -25,11 +26,11 @@ post('/dealerships') do
   name = params.fetch('name')
   Dealership.new(name).save()
   @dealerships = Dealership.all()
-  erb(:success)
+  erb(:success_dealership)
 end
 
 get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch('id'))
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
   erb(:vehicle)
 end
 
@@ -41,6 +42,10 @@ end
 get('/dealerships/:id/vehicles/new') do
   @dealership = Dealership.find(params.fetch('id').to_i())
   erb(:dealership_vehicles_form)
+end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
 end
 
 post('/vehicles') do
